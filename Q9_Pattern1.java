555bfe08c1e508f6173506c9f1b197e59b7aa5b2
diksharsasane <dikshasasane23@gmail.com>
2026-07-15 20:53:05 +0530

    added some butterfly and other patterns

--- Q9_Pattern1.java
+++ Q9_Pattern1.java
@@ -1,5 +1,89 @@
 public class Q9_Pattern1{
 
+    public void pattern22(int n) {
+        int size =(n*2)-1;
+        for(int i=0;i<size;i++){
+            for(int j=0;j<size;j++){
+               int top = i;
+               int left = j;
+               int bottom = size-i-1;
+               int right = size-j-1;
+               
+               int val = Math.min(Math.min(top,bottom),Math.min(left,right));
+
+               System.out.print((n-val)+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void pattern21(int n) {
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 || j==0 || i==(n-1) || j==(n-1)){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public void pattern20(int n) {
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+            for(int j=0;j<((n-i-1)*2);j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=n-1;i>0;i--){
+            for(int j=0;j<i;j++){
+                System.out.print("*");
+            }
+            for(int j=0;j<((n-i)*2);j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public void pattern19(int n) {
+        for(int i=n;i>0;i--){
+            for(int j=0;j<i;j++){
+                System.out.print("*");
+            }
+            for(int j=0;j<(n-i)*2;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+            for(int j=0;j<(n-i-1)*2;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
     public static void pattern18(int n) {
         for(int i=n;i>0;i--){
             for(int j=i-1;j<n;j++){
